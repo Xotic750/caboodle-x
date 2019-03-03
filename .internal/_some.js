252a140c423a817c971c69e41408ca78d40fb1ed
Graham Fairweather <xotic750@gmail.com>
2019-03-03 18:32:47 +0100

    wip

---
+++ .internal/_some.js
@@ -0,0 +1,9 @@
+/**
+ * @file Utility that needs description.
+ * @copyright Copyright (c) 2018-present, Graham Fairweather
+ * @module _some
+ */
+
+import _methodize from './_methodize';
+
+export default _methodize(Array.prototype.some);
